687f5198fe60ca382c94a416252faea1bf9ffe41
misraag <jaiofficial50@gmail.com>
2024-05-20 15:52:23 +0530

    created structure for user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+
+
 function App() {
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <main>
+      <div id="user-input">
+        <div className="input-group">
+          <p>
+            <label>INITIAL INVESTMENT</label>
+            <input type="text"></input>
+          </p>
+          <p>
+            <label>INITIAL INVESTMENT</label>
+            <input type="text"></input>
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label>INITIAL INVESTMENT</label>
+            <input type="text"></input>
+          </p>
+          <p>
+            <label>INITIAL INVESTMENT</label>
+            <input type="text"></input>
+          </p>
+        </div>
+      </div>
+      {/* <Result/> */}
+    </main>
+  );
 }
 
-export default App
+export default App;
